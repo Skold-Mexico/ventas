2f1c5f1dae2d7a0be1f605ae098bfcd5961bf458
Skold-Mexico <ia@skoldsafety.com>
2025-06-20 13:19:39 -0600

    nuevo cambio en frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hola este y ya sirve (espero ahora si) <br />
+          hola este y ya sirve (espero ahora si pipipi) <br />
         </p>
       </header>
     </div>
