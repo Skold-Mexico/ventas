07d19110a97acd54ba4076bd4747a2f1ea637393
Skold-Mexico <ia@skoldsafety.com>
2025-06-20 13:37:26 -0600

    nuevo commit en frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hola este y ya sirve (espero ahora si pipipi x2) <br />
+          hola ya pofavo <br />
         </p>
       </header>
     </div>
