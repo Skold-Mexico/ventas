6a25096a4659dd98006eb68845eebd72086abd0e
Skold-Mexico <ia@skoldsafety.com>
2025-06-20 13:10:45 -0600

    actualizo frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola cara de bola
+          hola este y ya sirve
         </p>
       </header>
     </div>
